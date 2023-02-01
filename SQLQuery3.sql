d0f264a090709fa50b2489c16adabf59e1efad1d
Rashmitakamble16 <119934172+Rashmitakamble16@users.noreply.github.com>
2023-02-01 12:46:33 +0530

    Delete SQLQuery3.sql

--- SQLQuery3.sql
+++
@@ -1,7 +0,0 @@
-select top (1000) [ProductId]
-			,[ProductName]
-			,[CategoryId]
-			,[CategoryName]
-		from [ProductsData].[dbo].[Products]
-use ProductsData;
-create table Products(ProductId int, ProductName varchar(255), CategoryId int, CategoryName varchar(255));
